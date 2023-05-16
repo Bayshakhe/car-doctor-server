d77c82626c0ae9ced9b2ea389452b0bb08b2b7e8
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-05-16 23:05:41 +0600

    verify with jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -19,6 +20,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error:true, message: 'Unauthorize access'})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(403).send({error: true, message: 'Unauthorize access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -26,6 +42,15 @@ async function run() {
 
     const servicesCollection = client.db("carDoctor").collection("services");
     const checkoutCollection = client.db("carDoctor").collection("checkout")
+
+    // JWT
+    app.post('/jwt', (req,res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({token})
+    })
 
     // SERVICES
     app.get('/services', async(req,res) => {
@@ -52,7 +77,12 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/checkout', async (req,res) => {
+    app.get('/checkout', verifyJWT, async (req,res) => {
+      const decoded = req.decoded;
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: 1, message: 'Forbidden access'})
+      }
+
       // console.log(req.query.email) 
       let query = {}
       if(req.query?.email){
